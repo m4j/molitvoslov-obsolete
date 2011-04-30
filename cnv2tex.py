c0279ac4070e17093ae31a07063103ba7f731093
Maxim Agapov <maxa@laptop1>
2011-04-30 16:22:23 -0400

    uzory; molitvoslov updates

--- cnv2tex.py
+++ cnv2tex.py
@@ -27,23 +27,25 @@ from urlparse import urlparse
 # enable utf-8 output
 sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
 
-sections = [u"", u"mychapter", u"section", u"subsection", u"subsubsection",
-    u"paragraph"]
+sections = [u'', u'mychapter', u'section', u'subsection', u'subsubsection',
+    u'paragraph']
 
-def fetch(level, baseUrl, path):
-    soup = getSoup(baseUrl, path)
+baseUrl = ''
+
+def fetch(level, path):
+    soup = getSoup(path)
     my_td = getMyTd(soup)
     if level > 0:
         heading = getHeading(my_td)
-        myprint("\n\n\\" + sections[level] + "{")
+        myprint('\n\n\\' + sections[level] + '{')
         myprint(heading)
-        myprint("}")
-    myprint("\n%" + baseUrl + path)
+        myprint('}')
+    myprint('\n%' + baseUrl + path)
     node = getNode(my_td)
     outputElement(getContents(node))
     subtitles = getSubtitles(node)
     for li in subtitles:
-        fetch(level + 1, baseUrl, li.a["href"])
+        fetch(level + 1, li.a['href'])
     #myprint(soup.prettify()
     return 0
 
@@ -51,14 +53,14 @@ def getHeading(td):
     return td.h1.next
     
 def getSubtitles(node):
-    subs = node.find("ul", { "class" : "nodehierarchy_title_list" })
+    subs = node.find('ul', { 'class' : 'nodehierarchy_title_list' })
     if subs:
         return subs
     else:
         return []
 
 def getNode(td):
-    return td.find("div", { "class" : "node" })
+    return td.find('div', { 'class' : 'node' })
     
 def getContents(node):
     return node.div.contents
@@ -69,7 +71,7 @@ def myprint(str):
 def outputElement(root, allow_br=True):
     for el in root:
         if isinstance(el, NavigableString):
-            #~ if el != "&nbsp;":
+            #~ if el != '&nbsp;':
             #~ s = el.replace('\n', ' ')
             s = el.replace('<', '$<$')
             s = s.replace('&lt;', '$<$')
@@ -78,34 +80,46 @@ def outputElement(root, allow_br=True):
             s = s.replace('&#8212;', '"---')
             s = s.replace('&quot;', '"')
             myprint(s)
-        elif el.name == "br" and allow_br:
-            myprint("\n\n")
-        elif el.name == "p":
-            myprint("\n\n")
-            if el.next == "&nbsp;":
-                myprint("\\medskip")
+        elif el.name == 'br' and allow_br:
+            myprint('\n\n')
+        elif el.name == 'p':
+            myprint('\n\n')
+            if el.next == '&nbsp;':
+                myprint('\\medskip')
             else:
                 outputElement(el, allow_br)
-        elif el.name == "i" or el.name == "em":
-            #~ if el.find("br"):
-                #~ myprint("\n\n");
-            myprint("\\itshape ")
+        elif el.name == 'i' or el.name == 'em':
+            #~ if el.find('br'):
+                #~ myprint('\n\n');
+            myprint('\\itshape ')
             outputElement(el, allow_br)
-            myprint("\\normalfont{}")
-        elif el.name == "b":
-            myprint("\\bfseries ")
+            myprint('\\normalfont{}')
+        elif el.name == 'b':
+            myprint('\\bfseries ')
             outputElement(el, allow_br)
-            myprint("\\normalfont{}")
+            myprint('\\normalfont{}')
+        elif el.name == 'a' and el.get('class') == 'icona':
+            bi_soup = getSoup(el['href'])
+            bi_div = bi_soup.find('div', { 'class' : 'big-icon' })
+            img_src = bi_div.img['src']
+            file_name = 'img/' + img_src.split('/')[-1]
+            img = urllib2.urlopen(img_src)
+            out = open(file_name, 'wb')
+            out.write(img.read())
+            out.close()
+            myprint('\\myfig{' + file_name + '}')
+            
     
 def getMyTd(soup):
-    return soup.find("div", id="center").table.tr.td.findNextSibling('td')
+    return soup.find('div', id='center').table.tr.td.findNextSibling('td')
     
-def getSoup(baseUrl, path = "/"):
+def getSoup(path = '/'):
     page = urllib2.urlopen(baseUrl + path)
     return BeautifulSoup(page)
 
 
 if __name__ == '__main__':
     url = urlparse(sys.argv[2])
-    fetch(int(sys.argv[1]), url.scheme + "://" + url.netloc, url.path)
+    baseUrl = url.scheme + '://' + url.netloc
+    fetch(int(sys.argv[1]), url.path)
 
