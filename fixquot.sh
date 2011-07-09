ab3449324f2090a4e340cfce78b35ec92caba2ff
Maxim Agapov <m4j@swissmail.org>
2011-07-09 13:29:10 -0400

    [Kazan] new files from import_20110709

--- fixquot.sh
+++ fixquot.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-#git checkout _*tex
-sed -i 's/"---/\&mdash;/g' _*tex
-sed -i 's/&quot;/"/g' _*tex
-sed -i 's/"\([[:alnum:]\.,:;(){}&?[:space:]]\+\)"/«\1»/g' _*tex
-grep -Hn '"' _*tex
-sed -i 's/\&mdash;/"---/g' _*tex
-sed -i 's/–/"---/g' _*tex
-sed -i 's/—/"---/g' _*tex
-sed -i 's/т\.е\./т.~е./g' _*tex
-sed -i 's/т\.д\./т.~д./g' _*tex
-sed -i 's/\.\.\./\ldots/g' _*tex
+#git checkout $1
+sed -i 's/"---/\&mdash;/g' $1
+sed -i 's/&quot;/"/g' $1
+sed -i 's/"\([[:alnum:]\.,:;(){}&?[:space:]]\+\)"/«\1»/g' $1
+sed -i 's/“/«/g' $1
+sed -i 's/”/»/g' $1
+grep -Hn '"' $1
+sed -i 's/\&mdash;/"---/g' $1
+sed -i 's/–/"---/g' $1
+sed -i 's/—/"---/g' $1
+sed -i 's/т\.е\./т.~е./g' $1
+sed -i 's/т\.д\./т.~д./g' $1
+sed -i 's/\.\.\./\ldots/g' $1
 
 #fix some invisible unicode characters
-sed -i 's/ //g' _*tex
-sed -i 's/­//g' _*tex
+sed -i 's/ //g' $1
+sed -i 's/­//g' $1
