2872e03d3f7eb267e68185b74f57bfd4339ada40
Maxim Agapov <m4j@swissmail.org>
2012-06-16 00:02:20 -0400

    [fetch_20120530] Completed import of the three canons.

--- fixirmos.sh
+++ fixirmos.sh
@@ -4,5 +4,6 @@ sed -i \
 -e 's/^\\itshape Ирмо.\+с:\\normalfont{} \(.\+\)$/\\irmos{\1}/' \
 -e 's/^\\itshape Припев:\\normalfont{} \(.\+\)$/\\pripev{\1}/' \
 -e 's/^Слава Отцу и Сыну и Святому Духу.$/\\slava/' \
--e 's/^И ныне и присно и во веки веков. Аминь.$/\\inyne/' $1
+-e 's/^И ныне и присно и во веки веков. Аминь.$/\\inyne/' \
+-e 's/^\\bfseries \(.\+\)\\normalfont{}$/\\mysubsubsection{\1}/' $1
 
