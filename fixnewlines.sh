a98c4ce884eca9cd11a39e044edbcbb5814a9782
Maxim Agapov <m4j@swissmail.org>
2012-08-16 16:33:59 -0400

    [fetch_20120530] two column layout changes -- not complete yet

---
+++ fixnewlines.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# we do not use \n in the substitute pattern of sed expression
+# because it is not supported on BSD systems, such as Mac OS X
+
+sed -E -n -i '' -e '
+# if the first line copy the pattern to the hold buffer
+1h
+# if not the first line then append the pattern to the hold buffer
+# and append newline
+1!H
+# if the last line then ...
+$ {
+        # copy from the hold to the pattern buffer
+        g
+        # do the search and replace
+        s/\n[[:space:]]*\n[[:space:]]*\n+/\
+\
+/g
+        # print
+        p
+}' "$1"
