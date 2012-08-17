2638a0276331031b67c67941992420d8349458a1
Maxim Agapov <m4j@swissmail.org>
2012-08-17 19:28:36 -0400

    [fetch_20120530] two column layout changes -- complete

--- fixitshape.sh
+++ fixitshape.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sed -n -i '
+# we do not use \n in the substitute pattern of sed expression
+# because it is not supported on BSD systems, such as Mac OS X
+
+sed -E -n -i '' -e '
 # if the first line copy the pattern to the hold buffer
 1h
 # if not the first line then append the pattern to the hold buffer
@@ -11,7 +14,9 @@ $ {
         # copy from the hold to the pattern buffer
         g
         # do the search and replace
-        s/\\itshape \n\+[[:space:]]*/\n\n\\itshape /g
+        s/\\itshape \n\n+[[:space:]]*/\
+\
+\\itshape /g
         # print
         p
-}' $1
+}' "$1"
