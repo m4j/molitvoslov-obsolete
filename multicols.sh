d19d9d4825ea9a6b4653df19274e375afcab6fe9
Maxim Agapov <m4j@swissmail.org>
2012-08-31 19:18:31 -0400

    [fetch_20120530] multicol completed for these files

--- multicols.sh
+++ multicols.sh
@@ -5,13 +5,16 @@ IN=$1
 cp "$IN" "$IN~" && sed \
   -e 's/mychapterending\[.*\]/mychapterending/' \
   -e 's/^\(.\+\)\\mychapterending/\1\n\n\\mychapterending/' \
-  -e 's/\\longpage{}//' -e 's/\\newpage//' \
+  -e 's/\\longpage{}//' \
+  -e 's/\\newpage//' \
   -e 's/myfig{/myfigure{/' \
   -e 's/myfig\[\(.*\)\]{/myfigure{/' \
   -e 's/myfigr{/myfigure{/' \
   -e 's/myfigr\[\(.*\)\]{/myfigure{/' \
   -e 's/myfigh{\(.*\)}{.*}/myfigure{\1}/' \
   -e 's/myfigh\[.*\]{\(.*\)}{.*}/myfigure{\1}/' \
+  -e 's/\\normalfont{//g' \
+  -e 's/\\itshape/\\myemph{/g' \
   -e 's/^\\section\[\(.*\)\].*/\\section{\1}/' "$IN~" |
     awk -f ins-multicols.awk > "$IN"
 
